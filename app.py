38b985fc1f3f63fef0238ea36e2669b324ed42a1
Joakim Olsson <barbatron@gmail.com>
2025-04-09 20:52:22 +0200

    feat: transmogrify into Slarren

--- app.py
+++ app.py
@@ -8,12 +8,17 @@ from listeners import register_listeners
 
 logging.basicConfig(level=logging.DEBUG)
 
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables
+
+
 # Initialization
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+app = App(token=os.environ.get("SLACK_BOT_USER_OAUTH_TOKEN"))
 
 # Register Listeners
 register_listeners(app)
 
 # Start Bolt app
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
+    SocketModeHandler(app, os.environ.get("SLACK_APP_LEVEL_TOKEN")).start()
--- listeners/messages/sample_message.py
+++ listeners/messages/sample_message.py
@@ -5,7 +5,7 @@ from slack_bolt import BoltContext, Say
 
 def sample_message_callback(context: BoltContext, say: Say, logger: Logger):
     try:
-        greeting = context["matches"][0]
-        say(f"{greeting}, how are you?")
+        # greeting = context["matches"][0]
+        say(f"talk to me again and I'll fucking kill you, {say.channel.capitalize()}")
     except Exception as e:
         logger.error(e)
